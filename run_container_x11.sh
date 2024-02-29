ecc193942a62788315f50344bdb54f28e2c3314b
Erik Tollerud <erik.tollerud@gmail.com>
2024-02-29 11:52:24 -0500

    work on local gpu with template

--- run_container_x11.sh
+++ run_container_x11.sh
@@ -14,6 +14,7 @@ $CONTAINER_EXEC run -it --rm --privileged $GPUS_OPTION \
            -e HOST_GID="$(id -g)" \
            -e PYDEVD_DISABLE_FILE_VALIDATION=1 \
            -e MAST_TOKEN=`cat ~/.mast_token` \
+           -e CUPY_CACHE_DIR=/containerapp/content/cupy_cache \
            --mount type=bind,source="$(pwd)"/content,target=/containerapp/content \
            `cat extramounts` \
            m31_jwst_2609 "$@"
